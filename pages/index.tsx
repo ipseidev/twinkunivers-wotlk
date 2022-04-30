51b6214ed14d1635e34fa58cdb8964ba150c8839
Nicolas Serra <serranicolas0904@gmail.com>
2022-04-30 20:09:32 +0200

    feat: animatin background

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
                     <h1 style={{color: "white", fontSize:"5rem", textShadow:"0px 3px 3px #144165"}}>Twinkunivers</h1>
                         <h3 style={{color: "#144165", fontSize:"2rem", textShadow:"3px 3px 3px black"}}>WOTLK edition</h3>
                     </div>
-                    <CarouselClasse/>
+                    {/*<CarouselClasse/>*/}
 
                 </div>
             </header>
