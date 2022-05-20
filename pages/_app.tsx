46e672efdb2f4dfbe98cca4047562f2485a9c9c3
Nicolas Serra <serranicolas0904@gmail.com>
2022-05-20 23:02:10 +0200

    fix: buiild sectoni

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,13 +20,13 @@ function MyApp({Component, pageProps}: AppProps) {
                 <meta property="og:url" content="https://www.twinkunivers.com/"/>
                 <meta property="og:title" content="Twinkunivers"/>
                 <meta property="og:description" content="Twinkunivers - Wotlk edition"/>
-                <meta property="og:image" content="/assets/background2.png"/>
+                <meta property="og:image" content="https://twinkunivers.com/assets/background2.png"/>
 
                 <meta property="twitter:card" content="summary_large_image"/>
                 <meta property="twitter:url" content="https://www.twinkunivers.com/"/>
                 <meta property="twitter:title" content="Twinkunivers"/>
                 <meta property="twitter:description" content="Twinkunivers - Wotlk edition"/>
-                <meta property="twitter:image" content="/assets/background2.png"/>
+                <meta property="twitter:image" content="https://twinkunivers.com/assets/background2.png"/>
             </Head>
             <Script
                 id="show-banner"
